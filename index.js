d73e382a410b0181bdd3e3d4110ffc18bb9c4adf
rifkan <mohamedrifkan00127@gmail.com>
2023-03-01 10:10:25 +0530

    removed unused code

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const fs = require("fs");
-const path = require("path");
 const dotenv = require("dotenv");
 const express = require("express");
 const connectDB = require("./src/config/connectDB");
@@ -15,20 +13,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 connectDB();
-
-// function loadEnabledPlugins() {
-//   fs.readFile(path.join(__dirname, "plugins.json"), "utf-8", (error, data) => {
-//     if (error) {
-//       console.log(error);
-//     } else {
-//       for (let key of Object.keys(JSON.parse(data))) {
-//         console.log(key);
-//       }
-//     }
-//   });
-// }
-
-// loadEnabledPlugins();
 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/products", productRoute);
