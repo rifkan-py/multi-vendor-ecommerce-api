d22398069bb83409529b58c491ef837b21878e95
rifkan <mohamedrifkan00127@gmail.com>
2023-03-01 15:08:19 +0530

    added authentication middleware

---
+++ src/middleware/authenticate.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+function authenticate(req, res, next) {
+  try {
+    const token = req.headers.authorization?.split(" ")[0];
+    if (!token) {
+      res.statusCode = 403;
+      throw new Error("Authorization denied.");
+    }
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    req.auth = {
+      id: decodedToken.id,
+    };
+    next();
+  } catch (exception) {
+    console.log(exception);
+    res.statusCode = exception.statusCode;
+    throw new Error(exception.message);
+  }
+}
+
+module.exports = authenticate;
